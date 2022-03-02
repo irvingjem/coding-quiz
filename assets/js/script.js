bd975775355b39c2729078800ba3b2b1a4272f87
Jem Irving <irving.jem@gmail.com>
2022-03-01 18:12:15 -0800

    added more sytling and functionality ie checkanswers sumbit and restarting the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,10 @@
 var questionNumber = 0;
 // var totalScore = 0;
 var timerEl = $("#timer")
+    //timer var
+var timeLeft = 30;
+var timeInterval;
+var score;
 
 var myQuestions = [{
         question: "Who invented JavaScript?",
@@ -10,7 +14,7 @@ var myQuestions = [{
             b: "Sheryl Sandberg",
             c: "Brendan Eich"
         },
-        correctAnswer: "c"
+        correctAnswer: "Brendan Eich"
     },
     {
         question: "Which one of these is a JavaScript package manager?",
@@ -32,40 +36,57 @@ var myQuestions = [{
     }
 ];
 
-// Questions array
-
 
 // timer for each question
 function countdown() {
-    var timeLeft = 60;
-
-    var timeInterval = setInterval(function() {
-        if (timeLeft < 1) {
-            clearInterval(timeInterval);
-            displayMessage();
-            timerEl.textContent = "";
-            endQuiz();
-        } else {
-            timerEl.textContent = timeLeft;
-            timeLeft--;
-        }
-    }, 1000);
-
-    // what ends the timer
-    // clearInterval(timerEnd);
+    console.log("Hello tik tok", timeLeft, timerEl);
+    if (timeLeft < 1) {
+        displayMessage();
+        timerEl.text("");
+        endQuiz();
+    } else {
+        timerEl.text(timeLeft);
+        timeLeft--;
+    }
 }
 
 function startQuiz() {
-
+    //starting timer
+    timeInterval = setInterval(countdown, 1000);
+    //displaying questions
     updateQuestionTitles(questionNumber);
-
-
     console.log("quiz has started")
     $(".hide").removeClass("hide");
+    //Hide the start screen 
+    $("#quiz").addClass("hide");
+    $("#results").addClass("hide")
 };
+
+function endQuiz() {
+    // what ends the timer
+    clearInterval(timeInterval);
+    alert("Game Over !!");
+    //show results 
+    $("#results").removeClass("hide");
+    //hide questions 
+    $("#questions").addClass("hide");
+}
+
+function submitScore() {
+    $("#results").addClass("hide");
+    $("#quiz").removeClass("hide");
+
+    //grab value from input field 
+    var name = $("#player-name").val();
+
+    localstorage.setItem(name, score);
+}
 
 // Loop function to replace text on BTNS
 function updateQuestionTitles(questionNumber) {
+
+    $("#question-title").text(myQuestions[questionNumber].question);
+
     console.log(myQuestions[questionNumber].answers.a)
     for (i = 0; i < 3; i++) {
         $("#answer1").text(myQuestions[questionNumber].answers.a)
@@ -77,17 +98,34 @@ function updateQuestionTitles(questionNumber) {
 //needs to check answers, update score if possible
 function checkAnswers() {
     //save answer given compare to correct answer 
-    console.log(this)
-    console.log("checkanswers running")
-    var answer = myQuestions
+    console.log("checkanswers running", $(this).text(), $(this))
+    var answer = myQuestions[questionNumber].correctAnswer;
+    console.log("answer", answer);
 
-    questionNumber++
-    updateQuestionTitles(questionNumber);
+    //Check the answer 
+    if ($(this).text() === answer) {
+        score = score + 5; //add score 
+    } else {
+        timeLeft = timeLeft - 5; //penality of 5 secs 
+    }
+
+    //Ran out of questions then endQuiz else move to next question 
+    if (questionNumber === myQuestions.length - 1) {
+        endQuiz();
+        //hide the 
+    } else {
+        questionNumber++ //Move to next index 
+        updateQuestionTitles(questionNumber);
+    }
+
+    console.log("Questions ", myQuestions.length, "current question number ", questionNumber)
 };
 
 // $("$answers")
 
 //Event Listeners
-$("#start").click(startQuiz)
-    // console.log(startQuiz)
-$(".choices").click(checkAnswers)
+$("#start").click(startQuiz);
+// console.log(startQuiz)
+$(".choices").click(checkAnswers);
+// Restarts and submits to local storage
+$("#submit").click(submitScore);
